d762675ea7aca66bd6c497b9f524ef9fec905865
D. Andrei Măceș <dmaces@nd.edu>
2015-07-25 14:37:53 -0400

    Add initial helper script
    Change-Id: I4704038d97ad62e9f1a989701630d58cb0bebfef

---
+++ envsetup.sh
@@ -0,0 +1,5 @@
+export ARCH=arm; export SUBARCH=arm
+
+#export CC=../../../prebuilts/gcc/linux-x86/arm/arm-eabi-4.8/bin/arm-eabi-
+export PATH=~/Android/REPO/prebuilts/gcc/linux-x86/arm/arm-eabi-4.8/bin:$PATH
+export CROSS_COMPILE=arm-eabi-
