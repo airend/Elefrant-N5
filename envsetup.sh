12f97aea0f9e72e029364e4f5fe09b2e04d98ef7
D. Andrei Măceș <dmaces@nd.edu>
2015-07-25 14:37:53 -0400

    Add a simple envsetup/helper script
    Change-Id: I4704038d97ad62e9f1a989701630d58cb0bebfef

---
+++ envsetup.sh
@@ -0,0 +1,6 @@
+export ARCH=arm; export SUBARCH=arm
+
+#export CC=../../../prebuilts/gcc/linux-x86/arm/arm-cortex_a15-linux-gnueabihf-linaro_4.9/bin/arm-eabi-
+export PATH=~/Android/REPO/prebuilts/gcc/linux-x86/arm/arm-cortex_a15-linux-gnueabihf-linaro_4.9/bin:$PATH
+#export PATH=~/Android/REPO/prebuilts/gcc/linux-x86/arm/arm-eabi-5.2/bin:$PATH
+export CROSS_COMPILE=ccache\ arm-eabi-
