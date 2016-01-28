d1f1f8257876cb06b527f98db3f703cec39ffa59
franciscofranco <franciscofranco.1990@gmail.com>
2016-01-28 01:13:45 +0000

    display: add a simple api to query the display state (on/off) at any point in time
    Signed-off-by: franciscofranco <franciscofranco.1990@gmail.com>
    
    Conflicts:
    	drivers/video/msm/mdss/mdss_dsi_panel.c
    
    Change-Id: I7de29503cf2dfab89f78165f3220f1c10ec6700c

--- drivers/video/msm/mdss/mdss_dsi_panel.c
+++ drivers/video/msm/mdss/mdss_dsi_panel.c
@@ -21,6 +21,7 @@
 #include <linux/leds.h>
 #include <linux/pwm.h>
 #include <linux/err.h>
+#include <linux/display_state.h>
 #ifdef CONFIG_TOUCHSCREEN_PREVENT_SLEEP
 #include <linux/input/prevent_sleep.h>
 #ifdef CONFIG_TOUCHSCREEN_SWEEP2WAKE
@@ -61,6 +62,13 @@ static struct work_struct send_cmds_work;
 struct mdss_panel_data *cmds_panel_data;
 static struct platform_driver this_driver;
 struct kobject *module_kobj;
+
+bool display_on = true;
+
+bool is_display_on()
+{
+	return display_on;
+}
 
 void mdss_dsi_panel_pwm_cfg(struct mdss_dsi_ctrl_pdata *ctrl)
 {
@@ -330,6 +338,8 @@ static int mdss_dsi_panel_on(struct mdss_panel_data *pdata)
 		return -EINVAL;
 	}
 
+	display_on = true;
+
 	ctrl = container_of(pdata, struct mdss_dsi_ctrl_pdata,
 				panel_data);
 	mipi  = &pdata->panel_info.mipi;
@@ -399,6 +409,9 @@ static int mdss_dsi_panel_off(struct mdss_panel_data *pdata)
 		mdss_dsi_panel_cmds_send(ctrl, &ctrl->off_cmds);
 
 	pr_info("%s:\n", __func__);
+
+	display_on = false;
+
 	return 0;
 }
 
