9a123906e663880e065b47652822df8fc99ed514
franciscofranco <franciscofranco.1990@gmail.com>
2016-04-06 04:05:00 +0000

    cpufreq: interactive: reduce timer with screen off
    Standard Qualcomm configuration on userspace's PowerHAL is to increase the timer_rate value to reduce power consumption. That doesn't seem to happen with Angler (or any other Nexus for that matter) so this patch implements it in the governor driver instead
    
    Tested with Google Play Music streaming in its higher bitrate, and there's no audio issues
    
    Signed-off-by: franciscofranco <franciscofranco.1990@gmail.com>
    
    Conflicts:
    	drivers/cpufreq/cpufreq_interactive.c
    
    Change-Id: I0a0cef1857ef94fb5caa2b1002e10d12955b2995

--- drivers/cpufreq/cpufreq_interactive.c
+++ drivers/cpufreq/cpufreq_interactive.c
@@ -29,6 +29,7 @@
 #include <linux/workqueue.h>
 #include <linux/kthread.h>
 #include <linux/slab.h>
+#include <linux/display_state.h>
 #include <asm/cputime.h>
 
 #define CREATE_TRACE_POINTS
@@ -87,6 +88,9 @@ static unsigned long min_sample_time = DEFAULT_MIN_SAMPLE_TIME;
  */
 #define DEFAULT_TIMER_RATE (20 * USEC_PER_MSEC)
 static unsigned long timer_rate = DEFAULT_TIMER_RATE;
+
+#define SCREEN_OFF_TIMER_RATE ((unsigned long)(50 * USEC_PER_MSEC))
+static unsigned long prev_timer_rate = DEFAULT_TIMER_RATE;
 
 /*
  * Wait this long before raising speed above hispeed, by default a single
@@ -279,6 +283,7 @@ static void cpufreq_interactive_timer(unsigned long data)
 	unsigned int loadadjfreq;
 	unsigned int index;
 	unsigned long flags;
+	bool display_on = is_display_on();
 	bool boosted;
 
 	if (!down_read_trylock(&pcpu->enable_sem))
@@ -294,6 +299,13 @@ static void cpufreq_interactive_timer(unsigned long data)
 
 	if (WARN_ON_ONCE(!delta_time))
 		goto rearm;
+
+	if (display_on && timer_rate != prev_timer_rate)
+		timer_rate = prev_timer_rate;
+	else if (!display_on && timer_rate != SCREEN_OFF_TIMER_RATE) {
+		prev_timer_rate = timer_rate;
+		timer_rate = max(timer_rate, SCREEN_OFF_TIMER_RATE);
+	}
 
 	do_div(cputime_speedadj, delta_time);
 	loadadjfreq = (unsigned int)cputime_speedadj * 100;
@@ -763,6 +775,7 @@ static ssize_t store_timer_rate(struct kobject *kobj,
 	if (ret < 0)
 		return ret;
 	timer_rate = val;
+	prev_timer_rate = val;
 	return count;
 }
 
